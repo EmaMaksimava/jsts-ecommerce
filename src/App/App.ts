1401412a5149f38d14436058f72c186347bf1497
Emma Maksimava <cielvibritania@gmail.com>
2022-10-16 13:13:24 +0200

    feat: create productList component

--- src/App/App.ts
+++ src/App/App.ts
@@ -1,6 +1,15 @@
+import { ProductsList } from "./Components/ProductsList";
+
 export class App {
 
+  private productList = new ProductsList();
+
   render() {
-    return `<h1>App E-Commerce</h1>`;
+    return `
+    <h1>App E-Commerce</h1>
+    <div>
+      ${this.productList.render()}
+    </div>
+    `;
   }
 }
---
+++ src/App/Components/ProductsList/ProductsList.ts
@@ -0,0 +1,6 @@
+export class ProductsList {
+
+  render() {
+    return `<h2> Product title</h2>`;
+  }
+}
