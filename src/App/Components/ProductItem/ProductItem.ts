7f8d1e2491862ee12126a156940c59f20c4818e5
Emma Maksimava <cielvibritania@gmail.com>
2022-10-17 09:50:38 +0200

    feat: create productItem component

---
+++ src/App/Components/ProductItem/ProductItem.ts
@@ -0,0 +1,12 @@
+export class ProductItem {
+
+  render() {
+    return `
+    <div>
+      <image>
+      <h3>Product name</h3>
+      <p>Price</p>
+    </div>
+    `
+  }
+}
--- src/App/Components/ProductsList/ProductsList.ts
+++ src/App/Components/ProductsList/ProductsList.ts
@@ -1,6 +1,27 @@
+import { ProductItem } from "../ProductItem";
+
+
+
 export class ProductsList {
 
+  private products: ProductItem[] = [
+    new ProductItem(),
+    new ProductItem(),
+    new ProductItem(),
+    new ProductItem(),
+    new ProductItem(),
+    new ProductItem(),
+
+  ]
+
   render() {
-    return `<h2> Product title</h2>`;
+    return `<h2> Product title</h2>
+    ${this.products.map(product => product.render()).join('')}
+    <div>
+      <button>prev</button>
+      <button>next</button>
+    </div>
+    `;
+
   }
 }
